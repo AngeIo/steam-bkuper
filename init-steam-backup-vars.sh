0bf111767e5a8b2b9a52dc6b74d96c5ed9184ff8
WisdomWolf <wisdomwolf@gmail.com>
2022-11-08 11:56:48 -0500

    Adds setup script and environment variables

---
+++ init-steam-backup-vars.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export STEAM_BKUPER_DIR="${STEAM_BKUPER_DIR:-$HOME/.local/share/steam-bkuper}"
+export STEAM_SAVES_DIR="${STEAM_SAVES_DIR:-$HOME/.local/share/steam-saves}"
+export STEAM_BKUPER_REPO="${STEAM_BKUPER_REPO:-'https://github.com/AngeIo/steam-bkuper'}"
+export STEAM_SAVES_REPO="${STEAM_SAVES_REPO:?}"
+export BIN_DIR=$HOME/bin
+
+mkdir -p $STEAM_SAVES_DIR
+mkdir -p $BIN_DIR
+
+# Uncomment for debugging
+# echo "STEAM_BKUPER_DIR = $STEAM_BKUPER_DIR"
+# echo "STEAM_SAVES_DIR = $STEAM_SAVES_DIR"
+# echo "STEAM_BKUPER_REPO = $STEAM_BKUPER_REPO"
+# echo "STEAM_SAVES_REPO = $STEAM_SAVES_REPO"
+# echo "BIN_DIR = $BIN_DIR"
---
+++ steam-backup-setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$(dirname `readlink -f $0`)
+$SCRIPT_DIR/init-steam-backup-vars.sh
+
+mkdir $BIN_DIR
+cd $HOME/bin
+wget https://github.com/mtkennerly/ludusavi/releases/download/v0.10.0/ludusavi-v0.10.0-linux.zip
+unzip ludusavi-v0.10.0-linux.zip
+rm ludusavi-v0.10.0-linux.zip
+
+git clone $STEAM_BKUPER_REPO $STEAM_BKUPER_DIR
+
+cp $STEAM_BKUPER_DIR/{my-steam-backup.sh,git-sync,init-steam-backup-vars.sh} $BIN_DIR
+chmod -R 755 $BIN_DIR
+rm -rf $STEAM_BKUPER_DIR
+cd $STEAM_SAVES_DIR
+git remote add origin-new $STEAM_SAVES_REPO
