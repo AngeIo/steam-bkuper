009885bf4e46e4cb8b466c26a6599e123b938390
WisdomWolf <wisdomwolf@gmail.com>
2022-11-08 12:20:59 -0500

    adds clone step for steam saves dir

--- steam-backup-setup.sh
+++ steam-backup-setup.sh
@@ -13,5 +13,8 @@ git clone $STEAM_BKUPER_REPO $STEAM_BKUPER_DIR
 cp $STEAM_BKUPER_DIR/{my-steam-backup.sh,git-sync,init-steam-backup-vars.sh} $BIN_DIR
 chmod -R 755 $BIN_DIR
 rm -rf $STEAM_BKUPER_DIR
+git clone $STEAM_SAVES_REPO $STEAM_SAVES_DIR
 cd $STEAM_SAVES_DIR
-git remote add origin-new $STEAM_SAVES_REPO
+if [ -n "$STEAM_SAVES_REPO2" ]; then
+    git remote add origin-new $STEAM_SAVES_REPO2
+fi
